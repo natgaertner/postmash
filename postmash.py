d58a69d2a4eef2f7eaf860998f0d8a5f8d3365de
EC2 Default User <ec2-user@ip-172-31-10-107.us-west-2.compute.internal>
2014-03-21 04:41:37 +0000

    adding dynamo talking

--- postmash.py
+++ postmash.py
@@ -1,9 +1,24 @@
 from flask import Flask
+from boto.dynamodb2.table import Table
+import json
+import logging
+from logging.handlers import RotatingFileHandler
 app = Flask(__name__)
+file_handler = RotatingFileHandler('/var/log/postmash/application.log')
+file_handler.setLevel(logging.WARNING)
+app.logger.addHandler(file_handler)
+posts = Table('posts')
+
 
 @app.route('/')
 def butt():
     return 'Butt'
+
+@app.route('/twoposts')
+def twoposts():
+    post1 = posts.get_item(postid='1')
+    post2 = posts.get_item(postid='2')
+    return json.dumps({'post1':dict(post1),'post2':dict(post2)})
 
 if __name__ == '__main__':
     app.run()
