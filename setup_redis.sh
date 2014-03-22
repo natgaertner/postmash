da6752478bb7dde284c867162696e8d7ebb6b14c
EC2 Default User <ec2-user@ip-172-31-10-108.us-west-2.compute.internal>
2014-03-22 09:11:27 +0000

    correctly activate virtualenv

--- setup_redis.sh
+++ setup_redis.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 (redis-server &> /dev/null &)
-(source /var/www/postmash/bin/activate)
+source /var/www/postmash/bin/activate
 echo 'butt'
 (python /var/www/postmash/postmash/copy_to_redis.py)
 echo 'butt2'
